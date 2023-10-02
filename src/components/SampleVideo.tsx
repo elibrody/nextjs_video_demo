bac5cd47613ad513d8f33b6e71f8c5868256c274
Jeff Trung <trungtrungkang@gmail.com>
2023-10-02 11:53:05 +0700

    Add Goback button

--- src/components/SampleVideo.tsx
+++ src/components/SampleVideo.tsx
@@ -1,6 +1,9 @@
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function SampleVideo(){
+  const router = useRouter();
+
   useEffect(() => {
     const el = document.getElementById('my_video_el') as HTMLVideoElement;
     if (el) {
@@ -20,6 +23,7 @@ export default function SampleVideo(){
         playsInline>
         <source src="https://download.samplelib.com/mp4/sample-5s.mp4" type="video/mp4" />
       </video>
+      <div><button onClick={() => router.push('/', undefined, {shallow: true})} style={{border: '2px solid black', padding: 15}}>Go Back</button></div>
     </div>
   );
 }
